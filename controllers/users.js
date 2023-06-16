7643f9452eef9030b8c3f52f33ecf8d334e96d9c
Sasha Dudina <aleksad301@gmail.com>
2023-06-16 14:25:40 +0300

    first

--- controllers/users.js
+++ controllers/users.js
@@ -1,94 +1,90 @@
 const User = require('../models/user');
 
 const getUsers = (req, res) => {
-    User.find({})
-    .then(users => res.status(200).send(users))
-    .catch(err => res.status(500).send({message: 'Internal Server Error', err: err.message, stack: err.stack}))
-}
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => res.status(500).send({ message: 'Internal Server Error', err: err.message, stack: err.stack }));
+};
 
 const getUserById = (req, res) => {
-
   User.findById(req.params.id)
-  .orFail(() => new Error('Not found'))
-  .then(user => res.status(200).send(user))
-  .catch((err) => {
-    if(err.message === 'Not found'){
+    .orFail(() => new Error('Not found'))
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Некорректные данные' });
+      } else if (err.message === 'Not found') {
         res
-        .status(404)
-        .send({
-          message: 'Пользователь не найден', 
-          })
-    } else
-      res
-      .status(500)
-      .send({
-         message: 'Internal Server Error', 
-         err: err.message, 
-         stack: err.stack})
-    })
-
-}
+          .status(404)
+          .send({
+            message: 'Пользователь не найден',
+          });
+      } else {
+        res.status(500).send({ message: 'Internal Server Error', name: err.name, err: err.message });
+      }
+    });
+};
 
 const createUser = (req, res) => {
-    const {name, about, avatar} = req.body;
+  const { name, about, avatar } = req.body;
 
-    User.create({name, about, avatar})
-    .then(user => res.status(200).send({data: user}))
+  User.create({ name, about, avatar })
+    .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
-      if(err.name === 'ValidationError'){
-        res.status(400).send({message: 'Некорректные данные'})
-      }else {
-        res.status(500).send({message: 'Internal Server Error', name: err.name, err: err.message})
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'Internal Server Error', name: err.name, err: err.message });
       }
-    })
-}
+    });
+};
 
 const changeUserInfo = (req, res) => {
   const filter = req.user;
   const change = req.body;
-  
-  User.findOneAndUpdate(filter, change, {new: true})
-  .then(user => res.status(200).send(user))
-  .catch((err) => {
-    if(err.name === 'ValidationError'){
-      res.status(400).send({message: 'Некорректные данные'})
-    }else if(err.message === 'Not found'){
-      res
-      .status(404)
-      .send({
-        message: 'Пользователь не найден', 
-    })
-  } else {
-      res.status(500).send({message: 'Internal Server Error', name: err.name, err: err.message})
-    }
-  })
-}
+
+  User.findOneAndUpdate(filter, change, { new: true })
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Некорректные данные' });
+      } else if (err.message === 'Not found') {
+        res
+          .status(404)
+          .send({
+            message: 'Пользователь не найден',
+          });
+      } else {
+        res.status(500).send({ message: 'Internal Server Error', name: err.name, err: err.message });
+      }
+    });
+};
 
 const changeAvatar = (req, res) => {
   const filter = req.user;
   const change = req.body;
-  
-  User.findOneAndUpdate(filter, change, {new: true})
-  .then(user => res.status(200).send(user))
-  .catch((err) => {
-    if(err.name === 'ValidationError'){
-      res.status(400).send({message: 'Некорректные данные'})
-    }else if(err.message === 'Not found'){
-      res
-      .status(404)
-      .send({
-        message: 'Пользователь не найден', 
-    })
-  } else {
-      res.status(500).send({message: 'Internal Server Error', name: err.name, err: err.message})
-    }
-  })
-}
+
+  User.findOneAndUpdate(filter, change, { new: true })
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Некорректные данные' });
+      } else if (err.message === 'Not found') {
+        res
+          .status(404)
+          .send({
+            message: 'Пользователь не найден',
+          });
+      } else {
+        res.status(500).send({ message: 'Internal Server Error', name: err.name, err: err.message });
+      }
+    });
+};
 
 module.exports = {
-    getUsers,
-    getUserById,
-    createUser,
-    changeUserInfo,
-    changeAvatar
+  getUsers,
+  getUserById,
+  createUser,
+  changeUserInfo,
+  changeAvatar,
 };
